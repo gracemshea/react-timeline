49384500d5c73b3bc32e21ad1a7330cd82640255
Grace Shea <grace.m.shea@gmail.com>
2019-06-10 15:37:54 -0700

    css thangs

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -14,7 +14,7 @@ const Timeline = (props) => {
   });
 
    return (
-    <section>
+    <section className="timeline">
       {timelineEventComponents}
     </section>
   )
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -5,10 +5,10 @@ import Timestamp from './Timestamp';
 const TimelineEvent = (props) => {
   // Fill in your code here
   return(
-    <section>
-      <h2> {props.person}</h2>
-      <p> {props.status}</p>
-      <p> <Timestamp
+    <section className="timeline-event">
+      <h2 className='event-person'> {props.person}</h2>
+      <p className="event-status"> {props.status}</p>
+      <p className="event-time"> <Timestamp
         time= {props.timeStamp}
         /></p>
     </section>
